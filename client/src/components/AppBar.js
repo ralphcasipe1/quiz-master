8bbb5123e2d3c3e4bd7066b1c909c5f80c350509
Ralph Kevin Casipe <ralphcasipe1@gmail.com>
2018-10-28 22:10:39 +0800

    Remove NavLink for launching the Quiz

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -4,44 +4,36 @@ import IconButton from '@material-ui/core/IconButton'
 import MuiAppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import AssignmentIcon from '@material-ui/icons/Assignment'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { withStyles } from '@material-ui/core/styles';
 import { styles } from '../helpers/styles'
 
-const AppBar = ({ classes }) => (
+const AppBar = ({ classes, location }) => (
   <MuiAppBar 
     className={classes.appBar}
     color="default"
     position="static"
   >
     <Toolbar className={classes.toolbar}>
-      <NavLink 
-        to="/"
-        exact
-        activeStyle={{ color: 'blue' }}
-        style={{ color: 'black'}}
-      >
-        <IconButton color="inherit" disableRipple>
-          <ArrowBackIcon />
-        </IconButton>
-      </NavLink>
-
+      { location !== '/'
+        ? (
+            <NavLink 
+              to="/"
+              exact
+              activeStyle={{ color: 'blue' }}
+              style={{ color: 'black'}}
+            >
+              <IconButton color="inherit" disableRipple>
+                <ArrowBackIcon />
+              </IconButton>
+            </NavLink>
+          )
+        : null
+      }
+      
       <Typography variant="h6" color="inherit" noWrap>
         Quiz Master
       </Typography>
-
-      <div>
-        <NavLink 
-          to="/quizes" 
-          activeStyle={{ color: 'blue' }}
-          style={{ color: 'black'}}
-        >
-          <IconButton color="inherit" disableRipple>
-            <AssignmentIcon />
-          </IconButton>
-        </NavLink>
-      </div>
     </Toolbar>
   </MuiAppBar>
 )
