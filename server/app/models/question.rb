e01eecb2f4933428d866b947302334c0968490ea
Ralph Kevin Casipe <ralphcasipe1@gmail.com>
2018-10-28 14:57:18 +0800

    add validations to  and  column

--- server/app/models/question.rb
+++ server/app/models/question.rb
@@ -1,2 +1,13 @@
 class Question < ApplicationRecord
+  validates :content, 
+    presence: true, 
+    uniqueness: { case_sensitive: false }, 
+    length: { 
+      minimum: 5, 
+      maximum: 300, 
+      too_long: "At most, %{count} characters", 
+      too_short: "At least, %{count} characters" 
+    }
+
+  validates :answer, presence: true
 end
